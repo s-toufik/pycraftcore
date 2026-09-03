c67b99909dbd32ca28f42fc72f8518094bc9121f
Toufik Saouchi <toufik.saouchi@gmail.com>
2026-09-03 18:06:05 +0200

    feat(file_handler): add CSV file reader and writer to adapter extension
    - Import CsvFileReader and CsvFileWriter in extension package
    - Add CsvFileReader and CsvFileWriter to __all__ exports list
    - Enable handling of CSV file operations alongside JSON and YML formats

--- src/pycraftcore/file_handler/adapter/extension/__init__.py
+++ src/pycraftcore/file_handler/adapter/extension/__init__.py
@@ -1,3 +1,5 @@
+from pycraftcore.file_handler.adapter.extension.csv.csv_reader import CsvFileReader
+from pycraftcore.file_handler.adapter.extension.csv.csv_writer import CsvFileWriter
 from pycraftcore.file_handler.adapter.extension.json.json_reader import JsonFileReader
 from pycraftcore.file_handler.adapter.extension.json.json_writer import JsonFileWriter
 from pycraftcore.file_handler.adapter.extension.yml.yml_reader import YmlFileReader
@@ -8,4 +10,6 @@ __all__ = [
     "YmlFileWriter",
     "JsonFileReader",
     "JsonFileWriter",
+    "CsvFileReader",
+    "CsvFileWriter",
 ]
