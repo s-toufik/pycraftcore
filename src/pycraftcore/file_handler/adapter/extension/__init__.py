ed4847bfce9e4da02de27480563fc414acc9c0d0
Toufik Saouchi <toufik.saouchi@gmail.com>
2026-09-06 17:47:26 +0200

    feat(file_handler): add support for markdown file extension
    - Import MarkdownFileReader and MarkdownFileWriter in extension adapter
    - Export MarkdownFileReader and MarkdownFileWriter in the module interface
    - Enable reading and writing of markdown files through the file handler adapter

--- src/pycraftcore/file_handler/adapter/extension/__init__.py
+++ src/pycraftcore/file_handler/adapter/extension/__init__.py
@@ -2,6 +2,8 @@ from pycraftcore.file_handler.adapter.extension.csv.csv_reader import CsvFileRea
 from pycraftcore.file_handler.adapter.extension.csv.csv_writer import CsvFileWriter
 from pycraftcore.file_handler.adapter.extension.json.json_reader import JsonFileReader
 from pycraftcore.file_handler.adapter.extension.json.json_writer import JsonFileWriter
+from pycraftcore.file_handler.adapter.extension.markdown.markdown_reader import MarkdownFileReader
+from pycraftcore.file_handler.adapter.extension.markdown.markdown_writer import MarkdownFileWriter
 from pycraftcore.file_handler.adapter.extension.yml.yml_reader import YmlFileReader
 from pycraftcore.file_handler.adapter.extension.yml.yml_writer import YmlFileWriter
 
@@ -12,4 +14,6 @@ __all__ = [
     "JsonFileWriter",
     "CsvFileReader",
     "CsvFileWriter",
+    "MarkdownFileWriter",
+    "MarkdownFileReader",
 ]
